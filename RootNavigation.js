31c7f3830249e03baf1031e0af3f271bb2289bc4
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-12-11 22:00:15 +0300

    React native stack navigation has been added.

---
+++ RootNavigation.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './screens/Home';
+import Splash from './screens/Splash';
+
+const Stack = createNativeStackNavigator();
+
+function RootNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default RootNavigation;
