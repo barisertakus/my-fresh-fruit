3d30e9f3e4e989874e72008eb8d6cc8450ad36ea
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-12-20 02:01:07 +0300

    Header has been added to Home screen.

---
+++ components/Home/HomeHeader.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import FontAwesome from "react-native-vector-icons/FontAwesome5";
+
+const HomeHeader = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttons}>
+        <TouchableOpacity>
+          <Icon name="sort" size={24} />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.basket}>
+          <FontAwesome name="shopping-basket" size={24} color="#FFA451" />
+          <Text style={styles.text}>My basket</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.header}>
+        <View style={{ flexDirection: "row" }}>
+          <Text style={styles.headerText}>Hello Baris, </Text>
+          <Text style={[styles.headerText, styles.textBold]}>
+            What fruit combo{" "}
+          </Text>
+        </View>
+        <Text style={[styles.headerText, styles.textBold]}>
+          do you want today?
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default HomeHeader;
+
+const styles = StyleSheet.create({
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+  },
+  text: {
+    color: "#27214D",
+    fontSize: 12,
+  },
+  basket: {
+    alignItems: "center",
+  },
+  header: {
+    marginTop: 20,
+  },
+  headerText: {
+    color: "#27214D",
+    fontSize: 18,
+  },
+  textBold: {
+    fontWeight: "600",
+  },
+});
--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,24 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import HomeHeader from "../components/Home/HomeHeader";
 
 const Home = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
-  )
-}
+    <SafeAreaView>
+      <View style={styles.container}>
+        <HomeHeader />
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default Home
+export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+    paddingTop: 20
+  }
+});
