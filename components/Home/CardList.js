85e15fd4076dec9bff732bd8a01027faf2d72261
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-12-20 23:44:47 +0300

    Category Fruits and Tab View have been added.

---
+++ components/Home/CardList.js
@@ -0,0 +1,110 @@
+import React from "react";
+import { ScrollView } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import FruitCard from "./FruitCard";
+
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle...
+  while (currentIndex != 0) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+const fruits = [
+  {
+    name: "Honey lime",
+    price: 2000,
+    image: <Image source={require("../../assets/images/fruit1.png")} />,
+  },
+  {
+    name: "Berry mango",
+    price: 8000,
+    image: <Image source={require("../../assets/images/fruit2.png")} />,
+  },
+  {
+    name: "Quinoa Salad",
+    price: 2000,
+    image: <Image source={require("../../assets/images/fruit3.png")} />,
+  },
+  {
+    name: "Tropical Fruit",
+    price: 2000,
+    image: <Image source={require("../../assets/images/fruit4.png")} />,
+  },
+  {
+    name: "Berry mango",
+    price: 8000,
+    image: <Image source={require("../../assets/images/fruit5.png")} />,
+  },
+  {
+    name: "Honey lime",
+    price: 2000,
+    image: <Image source={require("../../assets/images/fruit1.png")} />,
+  },
+  {
+    name: "Berry Mango",
+    price: 2000,
+    image: <Image source={require("../../assets/images/fruit1.png")} />,
+  },
+  {
+    name: "Quinoa Salad",
+    price: 8000,
+    image: <Image source={require("../../assets/images/fruit2.png")} />,
+  },
+  {
+    name: "Honey lime",
+    price: 2000,
+    image: <Image source={require("../../assets/images/fruit1.png")} />,
+  },
+];
+
+const CardList = ({ name }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>{name}</Text>
+      <ScrollView
+        style={styles.cards}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      >
+        {shuffle(fruits).map((fruit, i) => {
+          return (
+            <FruitCard
+              key={i}
+              name={fruit.name}
+              price={fruit.price}
+              image={fruit.image}
+            />
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default CardList;
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 20,
+  },
+  header: {
+    fontSize: 18,
+    color: "#27214D",
+    fontWeight: "600",
+  },
+  cards: {
+    marginTop: 20,
+  },
+});
---
+++ components/Home/CategoryFruits.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import { View, useWindowDimensions, StyleSheet, Text } from "react-native";
+import { TabView, SceneMap, TabBar } from "react-native-tab-view";
+import CardList from "./CardList";
+
+const FirstRoute = () => (
+  <View>
+    <CardList />
+  </View>
+);
+
+const SecondRoute = () => (
+  <View>
+    <CardList />
+  </View>
+);
+
+const ThirdRoute = () => (
+  <View>
+    <CardList />
+  </View>
+);
+
+const FourthRoute = () => (
+  <View>
+    <CardList />
+  </View>
+);
+
+const renderScene = SceneMap({
+  hottest: FirstRoute,
+  popular: SecondRoute,
+  new: ThirdRoute,
+  top: FourthRoute,
+});
+
+const renderTabBar = (props) => {
+  return (
+    <TabBar
+      activeColor="#27214D"
+      inactiveColor="#938DB5"
+      labelStyle={{
+        fontWeight: "600",
+        textTransform: "capitalize",
+        fontSize: 18,
+      }}
+      indicatorStyle={{
+        backgroundColor: "#FFA451",
+        paddingHorizontal: 20,
+      }}
+      style={styles.tabBar}
+      {...props}
+    />
+  );
+};
+
+function CategoryFruits() {
+  const layout = useWindowDimensions();
+
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: "hottest", title: "Hottest" },
+    { key: "popular", title: "Popular" },
+    { key: "new", title: "New" },
+    { key: "top", title: "Top" },
+  ]);
+  return (
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      renderTabBar={renderTabBar}
+      onIndexChange={setIndex}
+      initialLayout={{ width: layout.width }}
+    />
+  );
+}
+
+export default CategoryFruits;
+
+const styles = StyleSheet.create({
+  tabBar: {
+    backgroundColor: "white",
+    color: "black",
+    elevation: 0,
+    shadowOffset: { width: 0, height: 0 },
+    marginTop: 20,
+    marginHorizontal: 0,
+  },
+});
