405816e9c93c60fc00a0a68364d88a393ff1a033
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-12-24 04:18:15 +0300

    assets and Images helper have been added.

---
+++ Images.js
@@ -0,0 +1,55 @@
+export const images = [
+  {
+    name : "Meyve Cenneti",
+    src: require('./assets/images/fruit2.png'),
+  },
+  {
+    name : "Meyve Rüyası",
+    src: require('./assets/images/fruit3.png'),
+  },
+  {
+    name : "Meyve Şelalesi",
+    src: require('./assets/images/fruit4.png'),
+  },
+  {
+    name : "Yaz Rüyası",
+    src: require('./assets/images/yazsalatasi2.png'),
+  },
+  {
+    name : "Special",
+    src: require('./assets/images/fruit1.png'),
+  },
+  {
+    name : "Çıtır Peynirli Salata",
+    src: require('./assets/images/tadim.png'),
+  },
+  {
+    name : "Yaz Salatası",
+    src: require('./assets/images/yazsalatasi1.png'),
+  },
+  {
+    name : "Protein Deposu",
+    src: require('./assets/images/proteindeposu.png'),
+  },
+  {
+    name : "Leziz Meyveler",
+    src: require('./assets/images/fruit-detail.png'),
+  },
+  {
+    name : "Kırmızı Rüya",
+    src: require('./assets/images/fruit5.png'),
+  },
+  {
+    name : "Enerji Kaynağı",
+    src: require('./assets/images/enerjikaynagi.png'),
+  },
+  {
+    name : "Vitamin Kaynağı",
+    src: require('./assets/images/vitaminkaynagi1.png'),
+  },
+  {
+    name : "Karışık Special",
+    src: require('./assets/images/enerjikaynagi.png'),
+  },
+];
+
