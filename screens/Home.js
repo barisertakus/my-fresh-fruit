e7b311835cb3160c65525687651841244af93ca4
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-12-26 03:05:51 +0300

    To log in and add to cart or favorites, the Snackbar component has been added to the Home Screen.

--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,36 @@
 import React from "react";
+import { useLayoutEffect } from "react";
+import { useState } from "react";
 import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import SnackbarComponent from "react-native-snackbar-component";
+import { useDispatch, useSelector } from "react-redux";
 import CategoryFruits from "../components/Home/CategoryFruits";
 import HomeHeader from "../components/Home/HomeHeader";
 import RecommendedFruits from "../components/Home/RecommendedFruits";
 import SearchBar from "../components/Home/SearchBar";
+import { closeSnackbar, selectSnackbar } from "../features/snackBarSlice";
 
 const Home = ({ navigation }) => {
+  const snackbar = useSelector(selectSnackbar);
+  const dispatch = useDispatch();
+
   return (
     <View style={styles.background}>
       <SafeAreaView style={styles.safeArea}>
+        <View>
+        <SnackbarComponent
+            visible={snackbar.visible}
+            textMessage={snackbar.message}
+            actionHandler={() => dispatch(closeSnackbar())}
+            accentColor="blue"
+            position="top"
+            autoHidingTime={2000}
+            backgroundColor={snackbar.color}
+            messageColor="white"
+            actionText="Ok!"
+          />
+        </View>
         <ScrollView
           style={styles.container}
           contentContainerStyle={{ flexGrow: 1 }}
